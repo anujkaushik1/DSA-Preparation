8d127e4bba7cb46efec559207ca74baa0e1de1ab
Anuj Kaushik <66275103+anujkaushik1@users.noreply.github.com>
2024-05-22 06:45:56 +0530

    Create LinkedListClient.java

---
+++ LinkedList/Intro/LinkedListClient.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class LinkedListClient {
+    public static void main(String[] args) {
+
+        LinkedList ll = new LinkedList();
+        ll.addFirst(10);
+        ll.addFirst(40);
+        ll.addFirst(20);
+        ll.addFirst(80);
+
+        ll.display();
+    }
+}
