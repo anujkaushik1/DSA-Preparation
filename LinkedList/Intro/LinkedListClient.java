a763ffcf2a01ec6d4e54ff4638277e5ed2ec4653
Anuj Kaushik <66275103+anujkaushik1@users.noreply.github.com>
2024-05-22 08:15:23 +0530

    Update LinkedListClient.java

--- LinkedList/Intro/LinkedListClient.java
+++ LinkedList/Intro/LinkedListClient.java
@@ -4,11 +4,26 @@ public class LinkedListClient {
     public static void main(String[] args) {
 
         LinkedList ll = new LinkedList();
-        ll.addFirst(10);
-        ll.addFirst(40);
-        ll.addFirst(20);
         ll.addFirst(80);
+        ll.addFirst(70);
+        ll.addFirst(60);
+        ll.addFirst(50);
 
+//        ll.display();
+
+        ll.addLast(100);
+        ll.addLast(200);
+        ll.display();
+
+//        ll.insertAtIndex(28, 7);
+//        System.out.println();
+//        ll.display();
+
+//        ll.removeFirst();
+//        ll.removeLast();
+        System.out.println();
+        System.out.println(ll.removeAt(2));
+        System.out.println();
         ll.display();
     }
 }
