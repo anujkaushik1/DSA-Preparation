80a9c39ae09c24a4e31181d7c938c21fa012e0c4
Anuj Kaushik <66275103+anujkaushik1@users.noreply.github.com>
2024-05-23 08:20:37 +0530

    Update ConvertArrayToLinkedList.java

--- LinkedList/Intro/Basics/ConvertArrayToLinkedList.java
+++ LinkedList/Intro/Basics/ConvertArrayToLinkedList.java
@@ -1,15 +1,22 @@
 package LinkedList.Basics;
 
 import LinkedList.LinkedList;
+import LinkedList.DoublyLinkedList;
 
 public class ConvertArrayToLinkedList {
     public static void main(String[] args) {
         int arr[] = {2,4,6,8,20};
         LinkedList ll = new LinkedList();
+        DoublyLinkedList dl = new DoublyLinkedList();
         for(int val: arr){
             ll.addLast(val);
         }
+        for(int val : arr){
+            dl.addLast(val);
+        }
 
-        ll.display();
+        dl.display();
+
+//        ll.display();
     }
 }
