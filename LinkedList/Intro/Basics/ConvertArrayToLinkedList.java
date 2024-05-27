54b2422451fe9b042cac84f174a6648e236c528d
Anuj Kaushik <66275103+anujkaushik1@users.noreply.github.com>
2024-05-27 22:23:54 +0530

    Update ConvertArrayToLinkedList.java

--- LinkedList/Intro/Basics/ConvertArrayToLinkedList.java
+++ LinkedList/Intro/Basics/ConvertArrayToLinkedList.java
@@ -4,6 +4,7 @@ import LinkedList.LinkedList;
 import LinkedList.DoublyLinkedList;
 
 public class ConvertArrayToLinkedList {
+    // o(n^2) time complexity => add last function is taking (**HERE WE HAVE TO USE CONCEPT OF DUMMY NODE TO CREATE LL IN O(1)
     public static void main(String[] args) {
         int arr[] = {2,4,6,8,20};
         LinkedList ll = new LinkedList();
