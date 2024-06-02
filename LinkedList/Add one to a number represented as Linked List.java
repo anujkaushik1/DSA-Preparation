767b5059781e5bc8efe8c75d1c5f6bfea0c21a39
Anuj Kaushik <66275103+anujkaushik1@users.noreply.github.com>
2024-06-03 01:55:18 +0530

    Update Add one to a number represented as Linked List.java

--- LinkedList/Add one to a number represented as Linked List.java
+++ LinkedList/Add one to a number represented as Linked List.java
@@ -1,3 +1,77 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+
+    // head2 is greater
+    public ListNode collisionPoint(ListNode head1, ListNode head2, int mover){
+        
+        while(head2!= null && mover != 0){
+            head2 = head2.next;
+            mover--;
+        }
+
+        while(head1 != null){
+            
+            if(head1 == head2){
+                return head1;
+            }
+
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+        return null;
+
+    }
+
+    public ListNode getIntersectionNode(ListNode head1, ListNode head2) {
+        ListNode temp1 = head1;
+        int length1 = 0;
+        while(temp1 != null){
+            length1++;
+            temp1 = temp1.next;
+        }
+
+        ListNode temp2 = head2;
+        int length2 = 0;
+        while(temp2 != null){
+            length2++;
+            temp2 = temp2.next;
+        }
+
+        if(length2 > length1){
+            return collisionPoint(head1, head2, length2 - length1);
+        }
+
+        return collisionPoint(head2, head1, length1 - length2);
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import java.util.* ;
 import java.io.*; 
 /*************************************************************
