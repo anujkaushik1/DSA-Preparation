98799a8f509120f0366f41fd5690ee90a0c3d7a5
Anuj Kaushik <66275103+anujkaushik1@users.noreply.github.com>
2024-06-03 00:16:57 +0530

    Update Add one to a number represented as Linked List.java

--- LinkedList/Add one to a number represented as Linked List.java
+++ LinkedList/Add one to a number represented as Linked List.java
@@ -18,6 +18,73 @@ Following is the class structure of the Node class:
 
 public class Solution {
 
+	    public static int add1ToLLRecurive(Node head){
+
+         if(head == null){
+             return 1;
+         }
+
+         int carry = add1ToLLRecurive(head.next);
+         int rem = (head.data + carry) % 10;
+         int quo = (head.data + carry) / 10;
+         
+         head.data = rem;
+
+         carry = quo;
+
+         return carry;
+    }
+
+	public static Node addOne(Node head) {
+		
+         if(head == null)
+             return head;
+         
+         int carry = add1ToLLRecurive(head);
+         if(carry > 0){
+             Node firstNode = new Node(carry);
+             firstNode.next = head;
+             head = firstNode;
+         }
+         
+         return head;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import java.util.* ;
+import java.io.*; 
+/*************************************************************
+
+Following is the class structure of the Node class:
+
+    class Node {
+		public int data;
+		public Node next;
+
+		Node(int data) {
+			this.data = data;
+			this.next = null;
+	  	}
+    }
+
+*************************************************************/
+
+public class Solution {
+
 	    public static Node recursiveReverse(Node head){
          if(head == null || head.next == null){
              return head;
