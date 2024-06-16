59fb88eec4049f1b47f47225240f7e3ae944fff2
Anuj Kaushik <66275103+anujkaushik1@users.noreply.github.com>
2024-06-16 19:34:07 +0530

    Update Largest Area Histogram Optimized.java

--- Stacks/Largest Area Histogram Optimized.java
+++ Stacks/Largest Area Histogram Optimized.java
@@ -1,5 +1,43 @@
 class Solution {
     public int largestRectangleArea(int[] arr) {
+           Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        st.push(0);
+
+        int maxArea = 0;
+
+        for(int i = 1; i < arr.length; i++){
+            while (!st.isEmpty() && st.peek() != -1 && arr[i] < arr[st.peek()]){
+                int pop = st.pop();
+                int right = i;
+                int left = st.peek();
+
+                int height = arr[pop];
+                int width = right - left - 1;
+                maxArea = Math.max(maxArea, (height * width));
+            }
+
+            st.push(i);
+        }
+
+        while (!st.isEmpty() && st.peek() != -1){
+            int pop = st.pop();
+            int right = arr.length;
+            int left = st.peek();
+
+            int height = arr[pop];
+            int width = right - left - 1;
+            maxArea = Math.max(maxArea, (height * width));
+        }
+
+        return maxArea;
+    }
+}
+
+
+
+class Solution {
+    public int largestRectangleArea(int[] arr) {
                 Stack<Integer> st = new Stack<>();
         st.push(-1);
         st.push(0);
