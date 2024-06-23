70e855a76aa6e6c6645a4f24b66dc7b38824ecf2
Anuj Kaushik <66275103+anujkaushik1@users.noreply.github.com>
2024-06-23 18:32:43 +0530

    Update N-th Tribonacci Number.java

--- Recursion/N-th Tribonacci Number.java
+++ Recursion/N-th Tribonacci Number.java
@@ -1,4 +1,19 @@
 
+
+package Practice;
+
+public class Q4 {
+    static int nth(int n){
+        if(n == 0 || n < 0) return 0;
+        if(n ==1) return 1;
+        if(n == 2) return 1;
+
+        return nth(n- 1) + nth(n - 2) + nth(n - 3);
+    }
+    public static void main(String[] args) {
+        System.out.println(nth(25));
+    }
+}
 // 36/39 testcase passes -> TLE (have to use DP)
 class Solution {
       static int getTribonacci(int val){
