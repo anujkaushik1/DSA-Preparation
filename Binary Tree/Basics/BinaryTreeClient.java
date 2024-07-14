6a29a6122df6e806b50bec04657d716a2878d15b
Anuj Kaushik <66275103+anujkaushik1@users.noreply.github.com>
2024-07-14 20:07:08 +0530

    Create BinaryTreeClient.java

---
+++ Binary Tree/Basics/BinaryTreeClient.java
@@ -0,0 +1,14 @@
+package BinaryTrees;
+
+public class BinaryTreeClient {
+    public static void main(String[] args) {
+        BinaryTree bt = new BinaryTree();
+        bt.creation();
+
+//        bt.printPreorder();
+//        bt.printPostorder();
+//        bt.printInorder();
+//        System.out.println("root = "+ bt.root.data);
+
+    }
+}
